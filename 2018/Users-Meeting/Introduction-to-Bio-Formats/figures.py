4cb4e75e67f5375053bd6e93c5023087d6f37a4b
Melissa Linkert <melissa@glencoesoftware.com>
2018-05-02 23:23:24 -0500

    Fix X axis labels and legend layout

--- 2018/Users-Meeting/Introduction-to-Bio-Formats/figures.py
+++ 2018/Users-Meeting/Introduction-to-Bio-Formats/figures.py
@@ -7,26 +7,26 @@ df = pandas.read_csv('stats.csv')
 
 # classic users/formats/contribs chart
 plot.rc('axes', facecolor='whitesmoke')
-plot.rc('lines', linewidth=3, color='silver')
+plot.rc('lines', linewidth=3, color='silver', markersize=10)
 plot.rc('savefig', facecolor='whitesmoke')
 plot.grid()
+plot.tick_params('x', labelrotation=90)
+plot.tick_params('both', color='silver', labelcolor='#202020')
 
 # contribs
-plot.plot('Year', 'Contributors', data=df, marker='o', markersize=10, color='royalblue')
+plot.plot('Year', 'Contributors', data=df, marker='o', color='royalblue')
 
 # users
-plot.plot('Year', 'Users (thousands)', data=df, marker='o', markersize=10, color='gold')
+plot.plot('Year', 'Users (thousands)', data=df, marker='o', color='gold')
 
 # formats
-plot.plot('Year', 'Formats', data=df, marker='o', markersize=10, color='crimson')
+plot.plot('Year', 'Formats', data=df, marker='o', color='crimson')
 
-plot.figlegend(loc='lower center')
+plot.figlegend(loc='upper center', ncol=3)
 plot.savefig('stats.png')
 
 # TODO:
 # - classic chart:
-#   * rotate X labels 90 degrees
 #   * make plots continuous
 #   * fill in remainder of .csv
 #   * use different markers for each plot (in case colors aren't distinguishable)
-#   * make legend horizontal instead of vertical
