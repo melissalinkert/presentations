e457c9a955db8066b33ffaf99adf80e93c1b29f0
Melissa Linkert <melissa@glencoesoftware.com>
2018-05-23 20:26:36 -0500

    Add images/releases.png to plot releases per year

--- 2018/Users-Meeting/Introduction-to-Bio-Formats/figures.py
+++ 2018/Users-Meeting/Introduction-to-Bio-Formats/figures.py
@@ -1,19 +1,24 @@
 import matplotlib.pyplot as plot
 import pandas
 
+def setup_plot():
+    bgcolor = 'whitesmoke'
+    linecolor = 'silver'
+    plot.rc('axes', facecolor=bgcolor)
+    plot.rc('lines', linewidth=3, color=linecolor, markersize=10)
+    plot.rc('savefig', facecolor=bgcolor)
+    plot.grid()
+    plot.tick_params('x', labelrotation=90)
+    plot.tick_params('both', color=linecolor, labelcolor='#202020')
+
+
 # load from the csv
 
 df = pandas.read_csv('stats.csv')
 
 # classic users/formats/contribs chart
-bgcolor = 'whitesmoke'
-linecolor = 'silver'
-plot.rc('axes', facecolor=bgcolor)
-plot.rc('lines', linewidth=3, color=linecolor, markersize=10)
-plot.rc('savefig', facecolor=bgcolor)
-plot.grid()
-plot.tick_params('x', labelrotation=90)
-plot.tick_params('both', color=linecolor, labelcolor='#202020')
+
+setup_plot()
 
 # contribs
 plot.plot('Year', 'Contributors', data=df, marker='o', color='royalblue')
@@ -27,6 +32,15 @@ plot.plot('Year', 'Formats', data=df, marker='*', color='crimson')
 plot.figlegend(loc='upper center', ncol=3)
 plot.savefig('images/stats.png')
 
+# reset figure and generate releases over time
+
+plot.close()
+setup_plot()
+plot.plot('Year', 'Releases', data=df, marker='o', color='crimson')
+plot.figlegend(loc='upper center')
+plot.savefig('images/releases.png')
+
 # TODO:
 # - classic chart:
 #   * make plots continuous
+
